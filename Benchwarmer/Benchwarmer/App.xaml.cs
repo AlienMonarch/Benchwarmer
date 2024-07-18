7918fda16fa946cce6896c8c79c64016776886b9
Finn Tweedie <finntweedie07@gmail.com>
2024-07-18 14:54:27 +1000

    Created Project

---
+++ Benchwarmer/Benchwarmer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Benchwarmer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Benchwarmer/Benchwarmer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Benchwarmer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
