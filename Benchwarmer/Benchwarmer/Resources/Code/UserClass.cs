0e5cf85601ea03efec1f0664b7cfae7bd0b61666
Finn Tweedie <finntweedie07@gmail.com>
2024-08-05 10:14:03 +1000

    Started User Profile saving

---
+++ Benchwarmer/Benchwarmer/Resources/Code/UserClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchwarmer.Resources.Code
+{
+    internal class UserClass
+    {
+        private string name;
+        private string username;
+        private List<string> teams;
+        public UserClass() 
+        { 
+        
+        }
+        public void AddTeam(string name, Team team)
+        {
+
+        }
+    }
+}
--- Benchwarmer/Benchwarmer/obj/Debug/net8.0-android/Benchwarmer.AssemblyInfo.cs
+++ Benchwarmer/Benchwarmer/obj/Debug/net8.0-android/Benchwarmer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Benchwarmer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+58b01608bd93bc7bc5bad469e8daf9b6bc865287")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4333c1cd4a2d297cc381f7b1df31dc5e41f54b34")]
 [assembly: System.Reflection.AssemblyProductAttribute("Benchwarmer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Benchwarmer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
